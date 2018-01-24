5fd9a2f6758699e77abf69a15633d6a5d94cd45e
Zaid Shaikh <mzaidshaikh95@gmail.com>
2018-01-23 20:02:43 -0800

    Added comments to clear.js

--- Assignment 2/clear.js
+++ Assignment 2/clear.js
@@ -1,8 +1,10 @@
 var gl = null;
 
 function init() {
+	//finds the webgl-canvas in .html file
     var canvas = document.getElementById( "webgl-canvas" );
 
+	//Tells WebGLUtils to setup the canvas
     gl = WebGLUtils.setupWebGL( canvas );
 
     if ( !gl ) {
@@ -10,12 +12,14 @@ function init() {
         return;
     }
 
+	//Color for clearColor
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
 
     render();
 }
 
 function render() {
+	//Gives the entire canvas the clearColor defined before
     gl.clear( gl.COLOR_BUFFER_BIT );
 }
 
